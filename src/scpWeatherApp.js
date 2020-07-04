5f7fe3402613802fdf200cbd07532662d466e551
scmendez <30358609+scmendez@users.noreply.github.com>
2020-07-04 12:54:42 +0200

    Cleaning up Search and Current Location functions

--- src/scpWeatherApp.js
+++ src/scpWeatherApp.js
@@ -1,6 +1,5 @@
-//Current date and time
+//to update date and time, currently set to default (running on refresh)
 let now = new Date();
-let currentDate = document.querySelector("#current-date");
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
@@ -32,60 +31,49 @@ let min = now.getMinutes();
 if (min < 10) {
   min = `0${min}`;
 }
+
+let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${day}, ${month} ${date}, ${year} <br /> ${hour}:${min}`;
 
-//Display city and weather
-function showCelTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let tempElement = document.querySelector("#temp-element");
-  tempElement.innerHTML = `${temp}`;
-}
-
-function callTemp() {
-  let apiKey = "2f036bba5972d2593243a4f078d73ef2";
-  let searchCityInput = document.querySelector("#search-city-input");
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCelTemp);
-}
-
-function displayCurrentLocationTemp(response) {
+//main function updating everything
+function updateLocationAndConditions(response) {
   document.querySelector("#searched-city").innerHTML = response.data.name;
   document.querySelector("#temp-element").innerHTML = Math.round(
     response.data.main.temp
   );
 }
 
-function searchCity(event) {
-  event.preventDefault();
-
-  let searchCityInput = document.querySelector("#search-city-input");
-  let h1City = document.querySelector("#searched-city");
-  if (searchCityInput.value) {
-    h1City.innerHTML = `${searchCityInput.value}`;
-  } else {
-    h1City.innerHTML = null;
-  }
-
-  callTemp();
+//submit and search button functions
+function searchCity(city) {
+  let apiKey = "2f036bba5972d2593243a4f078d73ef2";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(updateLocationAndConditions);
 }
 
-function searchLocation(position) {
+function submittedCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  searchCity(city);
+}
+
+//current location functions
+function searchCurrentLocation(position) {
   let apiKey = "2f036bba5972d2593243a4f078d73ef2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayCurrentLocationTemp);
+  axios.get(apiUrl).then(updateLocationAndConditions);
 }
 
 function getCurrentLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
 }
 
+//event listeners
 let searchCityForm = document.querySelector("#search-city");
-searchCityForm.addEventListener("submit", searchCity);
+searchCityForm.addEventListener("submit", submittedCity);
 
 let searchCityButton = document.querySelector("#search-city-button");
-searchCityButton.addEventListener("click", searchCity);
+searchCityButton.addEventListener("click", submittedCity);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
